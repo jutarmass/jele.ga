7b6dddcabc67f081349ea4195c28d6bb1c568b71
jutarmass <jkhongjang@gmail.com>
2020-08-02 13:57:17 +0700

    add build

---
+++ build/precache-manifest.c65d9367e46fc44add089eac05862481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11577ec9da426064c70e8cac88f3c398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60628393bb555f59ec6b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c3eacb4557883f9b5e1",
+    "url": "/static/js/2.28829bed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28829bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60628393bb555f59ec6b",
+    "url": "/static/js/main.ebb3c293.chunk.js"
+  },
+  {
+    "revision": "97c268acbd4c49c87ad0",
+    "url": "/static/js/runtime-main.3ece60e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
